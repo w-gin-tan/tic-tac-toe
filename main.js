a30f1aaa9fe5076c960bc3b2efd84a301084ac84
Wayne Tan <wtg28@outlook.com.au>
2023-08-22 12:03:02 +0800

    Refactor & add factories + modules

--- main.js
+++ main.js
@@ -1,20 +1,63 @@
-const gameBoard = (() => {
-    const initBoard = () => ['O', 'O', 'O',
-                             'X', 'X', 'X',          
-                             'O', 'O', 'X'];
+const Cell = (val = "") => {
+    const _value = val;
+
+    const setValue = (val) => {
+        _value = val;
+    }
+    
+    const getValue = () => {
+        return _value;
+    }
+
     return {
-        initBoard
+        setValue,
+        getValue
+    };
+};
+
+const Player = (turn) => {
+    const player = turn;
+
+    // setScore, getScore?
+
+    return {
+        player
+    };
+};
+
+const gameBoard = (() => {
+    const _board = new Array(9).fill(Cell(Player('X')).getValue());
+
+    const init = () => {
+        // for loop create cells
+        // setup with DOM onclick elements connected to each cell in init
+    };
+
+    const getBoard = () => {
+        return _board;
+    }
+
+    return {
+        init,
+        getBoard
     };
 })();
 
 const gameController = (() => {
-    const board = gameBoard.initBoard();
 
-    const showBoardState = () => board;
+    // all this stuff in displaycontroller
+    gameBoard.init();
+
+    // const playerX = Player('X');
+    // const playerY = Player('Y');
+
+    // setup game board
+    // DOM display board and click functions
+
+    showBoardState = gameBoard.getBoard();
+    console.log(showBoardState);
 
     return {
         showBoardState
     };
 })();
-
-console.log(gameController.showBoardState());
